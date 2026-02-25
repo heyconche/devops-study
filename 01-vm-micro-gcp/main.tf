b7e47abe0b1097d259096fd64217cc753c7b2342
heyconche <joaoconche@hotmail.com>
2026-02-25 18:30:22 -0300

    initial vm setup

---
+++ 01-vm-micro-gcp/main.tf
@@ -0,0 +1,27 @@
+# 1. Cria a rede VPC
+resource "google_compute_network" "vpc_rede_estudo" {
+  name                    = "rede-estudos"
+  auto_create_subnetworks = true
+}
+
+# 2. Cria a VM usando a rede acima
+resource "google_compute_instance" "vm_teste" {
+  name         = "vm-hostname-x"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # Aqui conectamos a VM na rede que criamos acima
+    network = google_compute_network.vpc_rede_estudo.name
+    
+    access_config {
+      
+    }
+  }
+}
---
+++ 01-vm-micro-gcp/provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project = "lab-demo-backup"
+  region  = "us-central1"
+}
